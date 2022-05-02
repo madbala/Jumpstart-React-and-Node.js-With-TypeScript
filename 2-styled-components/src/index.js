12c5579c460d628f80921d0f7d6d25ec0e2b8893
Balaguru Ravi <balaguru@litmus7.com>
2022-05-01 20:45:28 -0700

    proj 2 - task done

--- 2-styled-components/src/index.js
+++ 2-styled-components/src/index.js
@@ -1,8 +1,55 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components";
 
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px'
+}
+
+const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`
+};
+
+
+const Header = styled.h1`
+@media ${device.mobileS} { 
+  color:green;
+}
+
+@media ${device.mobileM} {
+  color:green
+}
+
+@media ${device.mobileL} { 
+  color:green;
+}
+@media ${device.laptop} {
+  color:red
+}
+
+@media ${device.desktop} {
+  color:red
+}
+`;
+const Hero = styled(Header)`color:blue;font-size:100px`;
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return <>
+  <Hero>I am a hero</Hero>
+  <Header>Hello React - Task 2!</Header>
+  </>
 };
 
 const rootElement = document.getElementById("root");
